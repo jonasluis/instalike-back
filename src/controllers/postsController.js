0978d95503cfa33d6c1026253d8bba84de35472d
jonasluis <jonasluis66@gmail.com>
2024-11-20 10:30:06 -0300

    criado uma classe para controlar as respostas

---
+++ src/controllers/postsController.js
@@ -0,0 +1,6 @@
+export async function listarPosts(req, res){
+    // Chama a função getTodosPosts para obter todos os posts do db.
+    const posts = await getTodosPosts();
+    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON.
+    res.status(200).json(posts);
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,16 +1,12 @@
 import express from "express";
+import { listarPosts } from "../controllers/postsController.js";
 
 const routes = (app) => {
     //Permite que o servidor interprete as requisições com corpo JSON. Tranforma tudo que da em json
     app.use(express.json());
 
     // Define uma rota GET para a URL "/posts".
-    app.get("/posts", async (req, res) => {
-        // Chama a função getTodosPosts para obter todos os posts do db.
-        const posts = await getTodosPosts();
-        // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON.
-        res.status(200).json(posts);//envia os objetos
-    }) ;
+    app.get("/posts", listarPosts);
 }
 
 export default routes;
