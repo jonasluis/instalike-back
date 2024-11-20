e98b3ac0c517bb71a97ac13a9bc974ff169047f8
jonasluis <jonasluis66@gmail.com>
2024-11-20 10:18:50 -0300

    Refatorando os comentarios do codigo e add a funcao para buscar os dados do banco de dados

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
 
-const posts = [
-    { 
-        descricao: "Uma foto teste", 
-        imagem: "https://placecats.com/millie/300/150", 
-        id: 1 
-    },
-    { 
-        descricao: "Gato fofo dormindo", 
-        imagem: "https://placecats.com/millie/300/150",
-        id: 2
-    },
-    {
-        descricao: "Gato olhando pela janela",
-        imagem: "https://placecats.com/millie/300/150",
-        id: 3
-    }
-];
+// Cria uma conexão com o banco de dados, utilizando a string de conexão fornecida como variável de ambiente.
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
-const app = express(); // far a constant app ser o servidor
-app.use(express.json()); // tranforma tudo que da em json
-app.listen(3000, () => { // inicia o server na porta 3000
+// Inicializa uma aplicação Express.js
+const app = express(); 
+//Permite que o servidor interprete as requisições com corpo JSON. Tranforma tudo que da em json
+app.use(express.json());
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver ouvindo.
+app.listen(3000, () => { 
     console.log("servidor escutando");
 }); 
 
-app.get("/posts", (req, res) => {// define a rota com todos objetos
+// Função assíncrona para obter todos os posts do banco de dados.
+async function getTodosPosts() {
+    // conecta ao banco de dados "imersao-instabytes"
+    const db = conexao.db("imersao-instabytes"); 
+    // Seleciona a coleção "posts" dentro do banco de dados.
+    const colecao = db.collection("posts"); 
+    // Retorna todos os documentos (posts) da coleção como um array.
+    return colecao.find().toArray()
+}
+// Define uma rota GET para a URL "/posts".
+app.get("/posts", async (req, res) => {
+    // Chama a função getTodosPosts para obter todos os posts do db.
+    const posts = await getTodosPosts();
+    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON.
     res.status(200).json(posts)//envia os objetos
 }) ;
 
-function buscarPostPorID(id) {
-    return posts.findIndex((post) => { //pega um post dentro dos posts
-        return post.id === Number(id) //verifica se o id eh igual ao id que passou no parametro
-    })
-}
+// function buscarPostPorID(id) {
+//     return posts.findIndex((post) => { //pega um post dentro dos posts
+//         return post.id === Number(id) //verifica se o id eh igual ao id que passou no parametro
+//     })
+// }
 
 
-app.get("/posts/:id", (req, res) => {// define a rota com um id especifico
-    const index = buscarPostPorID(req.params.id); // envia o id que passamos como parametro
-    res.status(200).json(posts[index]); //retorna o objeto do post com base no id
-}) 
+// app.get("/posts/:id", (req, res) => {// define a rota com um id especifico
+//     const index = buscarPostPorID(req.params.id); // envia o id que passamos como parametro
+//     res.status(200).json(posts[index]); //retorna o objeto do post com base no id
+// }) 
