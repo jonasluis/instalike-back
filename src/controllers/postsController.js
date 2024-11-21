21d096c9733088d8a34fc15fffc227714daa2709
jonasluis <jonasluis66@gmail.com>
2024-11-21 19:15:18 -0300

    criado a funcao para postar novos posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,8 +1,20 @@
-import getTodosPosts from "../models/postsModel.js";
+import {getTodosPosts, criarPost} from "../models/postsModel.js";
 
 export async function listarPosts(req, res){
     // Chama a função getTodosPosts para obter todos os posts do db.
     const posts = await getTodosPosts();
     // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON.
     res.status(200).json(posts);
+}
+
+export async function postarPost(req, res) {
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro:":"Falha na requisicão"})
+    }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import { listarPosts,  postarPost } from "../controllers/postsController.js";
 
 const routes = (app) => {
     //Permite que o servidor interprete as requisições com corpo JSON. Tranforma tudo que da em json
@@ -7,6 +7,7 @@ const routes = (app) => {
 
     // Define uma rota GET para a URL "/posts".
     app.get("/posts", listarPosts);
+    app.post("/posts",  postarPost);
 }
 
 export default routes;
