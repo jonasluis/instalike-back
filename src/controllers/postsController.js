38c19a202c8af53470261429e3dcc18544d31638
jonasluis <jonasluis66@gmail.com>
2024-11-23 09:59:48 -0300

    refatorando o codigo

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,79 +1,60 @@
-import fs from "fs"
 import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
+import fs from "fs";
 import gerarDescricaoComGemini from "../services/geminiService.js"
 
-
-export async function listarPosts(req, res){
-    // Chama a função getTodosPosts para obter todos os posts do db.
+export async function listarPosts(req, res) {
+    // Chama a função para buscar os posts
     const posts = await getTodosPosts();
-    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON.
+    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON
     res.status(200).json(posts);
 }
 
-export async function postarPost(req, res) {
-    // Extrai os dados do novo post a partir do corpo da requisição
+export async function postarNovoPost(req, res) {
     const novoPost = req.body;
-
     try {
-        // Chama a função `criarPost` para inserir o novo post no banco de dados
-        // A função `criarPost` deve estar definida em outro módulo e lidar com a lógica de inserção
         const postCriado = await criarPost(novoPost);
-        // Retorna uma resposta HTTP 200 com o objeto do post criado como JSON
-        res.status(200).json(postCriado);
-    } catch (erro) {
-        // Imprime o erro no console para facilitar a depuração
+        res.status(200).json(postCriado);  
+    } catch(erro) {
         console.error(erro.message);
-        // Retorna uma resposta HTTP 500 com uma mensagem de erro genérica
-        res.status(500).json({"Erro:":"Falha na requisicão"})
+        res.status(500).json({"Erro":"Falha na requisição"})
     }
 }
 
 export async function uploadImagem(req, res) {
-    // Cria um objeto com os dados do novo post, incluindo a URL da imagem
     const novoPost = {
-      descricao: "",
-      imgUrl: req.file.originalname, // Considerar usar um nome único para a imagem
-      alt: ""
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
     };
-    try {
-      // Cria o post no banco de dados
-      const postCriado = await criarPost(novoPost);
-      // Constrói o caminho completo para a imagem
-      const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
-      // Move a imagem para o destino final
-      fs.renameSync(req.file.path, imagemAtualizada);
-      // Retorna uma resposta HTTP 200 com o objeto do post criado como JSON
-      res.status(200).json(postCriado);
-    } catch (erro) {
-      // Imprime o erro no console para facilitar a depuração
-      console.error(erro.message);
-      // Retorna uma resposta HTTP 500 com uma mensagem de erro genérica
-      res.status(500).json({"Erro":"Falha na requisição"})
-    }
-  }
-
-  export async function atualizarNovoPost(req, res) {
-    const id = req.params.id;
-    const urlImagem = `http://localhost:3000/${id}.png`;
-    
 
     try {
-      
-      const imageBuffer = fs.readFileSync(`uploads/${id}.png`);
-      const descricao = await gerarDescricaoComGemini(imageBuffer);
-      const post = {
-        imgUrl: urlImagem,
-        descricao: descricao,
-        alt: req.body.alt
-      };
-      const postCriado = await atualizarPost(id, post);
-     
-        // Retorna uma resposta HTTP 200 com o objeto do post criado como JSON
-        res.status(200).json(postCriado);
-    } catch (erro) {
-        // Imprime o erro no console para facilitar a depuração
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
         console.error(erro.message);
-        // Retorna uma resposta HTTP 500 com uma mensagem de erro genérica
-        res.status(500).json({"Erro:":"Falha na requisicão"});
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`
+    try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer)
+
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+
+        const postCriado = await atualizarPost(id, post);
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
     }
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,18 +1,17 @@
 import 'dotenv/config';
 import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
+// Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-// Cria uma conexão com o banco de dados, utilizando a string de conexão fornecida como variável de ambiente.
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
-
-// Função assíncrona para obter todos os posts do banco de dados.
+// Função assíncrona para buscar todos os posts do banco de dados
 export async function getTodosPosts() {
-    // conecta ao banco de dados "imersao-instabytes"
-    const db = conexao.db("imersao-instabytes"); 
-    // Seleciona a coleção "posts" dentro do banco de dados.
-    const colecao = db.collection("posts"); 
-    // Retorna todos os documentos (posts) da coleção como um array.
-    return colecao.find().toArray()
+    // Seleciona o banco de dados "imersao-instabytes"
+    const db = conexao.db("imersao-instabytes");
+    // Seleciona a coleção "posts" dentro do banco de dados
+    const colecao = db.collection("posts");
+    // Retorna um array com todos os documentos da coleção
+    return colecao.find().toArray();
 }
 
 export async function criarPost(novoPost) {
